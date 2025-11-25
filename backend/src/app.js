4537d472d54c700ad8a0eafb0527186dc2672723
Unai Egusquiza <unai.egusquizadel@opendeusto.es>
2025-11-25 19:13:47 +0100

    [Backend] Routes creation and "Tareas" new functions

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,12 @@ import cors from 'cors';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import userRoutes from './routes/userRoutes.js';
-
+import actividadRoutes from './routes/actividadRoutes.js';
+import colaboradorRoutes from './routes/colaboradorRoutes.js';
+import empleadoRoutes from './routes/empleadoRoutes.js';
+import noticiaRoutes from './routes/noticiaRoutes.js';
+import tareaRoutes from './routes/tareaRoutes.js';
+import residenteRoutes from './routes/residenteRoutes.js';
 const app = express();
 
 // Middlewares
@@ -14,5 +19,11 @@ app.use(rateLimit({windowMs: 15 * 60 * 1000, max: 100}));
 
 // Rutas
 app.use('/api/users', userRoutes);
+app.use('/api/colaboradores', colaboradorRoutes);
+app.use('/api/empleados', empleadoRoutes);
+app.use('/api/noticias', noticiaRoutes);
+app.use('/api/tareas', tareaRoutes);
+app.use('/api/residentes', residenteRoutes);
+app.use('/api/actividades', actividadRoutes);
 
 export default app;
--- backend/src/models/Tarea.js
+++ backend/src/models/Tarea.js
@@ -21,6 +21,24 @@ const Tarea = {
     return result.rows[0];
   },
 
+  getById: async (id) => {
+    const result = await pool.query("SELECT * FROM tareas WHERE id = $1", [id]);
+    return result.rows[0];
+  },
+
+  update: async (id, tareaData) => {
+    const query = `
+      UPDATE tareas SET titulo=$1, descripcion=$2, estado=$3, asignado_a=$4
+      WHERE id=$5 RETURNING *;
+    `;
+    const values = [
+      tareaData.titulo, tareaData.descripcion, tareaData.estado,
+      tareaData.asignado_a, id
+    ];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  },
+
   updateStatus: async (id, estado) => {
     const result = await pool.query(
       "UPDATE tareas SET estado=$1 WHERE id=$2 RETURNING *",
